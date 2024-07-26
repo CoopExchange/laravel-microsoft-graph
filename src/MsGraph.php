e260c62f17e94d38ff3e9536bf3dd0e4428e8159
Joe Sandford-Hughes <joe.sandford-hughes@coop.exchange>
2024-07-26 15:28:56 +0100

    Update MsGraph.php

--- src/MsGraph.php
+++ src/MsGraph.php
@@ -172,7 +172,7 @@ class MsGraph
         $id = $this->getUserId($id);
 
         if ($redirectWhenNotConnected) {
-            if (! $this->isConnected()) {
+            if (! $this->isConnected($id)) {
                 return redirect()->away(config('msgraph.redirectUri'));
             }
         }
@@ -197,7 +197,7 @@ class MsGraph
         return MsGraphToken::where('user_id', $id)->where('refresh_token', '<>', '')->first();
     }
 
-    public function storeToken(string $access_token, string $refresh_token, string $expires, int $id, string $email): MsGraphToken
+    public function storeToken(string $access_token, string $refresh_token, string $expires, string $id, string $email): MsGraphToken
     {
         return MsGraphToken::updateOrCreate(['user_id' => $id], [
             'user_id' => $id,
